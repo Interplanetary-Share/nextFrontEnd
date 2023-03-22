9fc805b105bee4f0075607b94fb120d13ba58474
kenneth suarez <33601506+polopopeye@users.noreply.github.com>
2023-03-22 18:58:25 +0100

    set default darktheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,4 +16,15 @@ module.exports = {
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dark',
+    themes: ['dark'],
+  },
 };

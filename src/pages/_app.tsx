16853f720ecb248f05e1dc7ca8ce5ad49cde60ba
Kenneth Suarez <33601506+polopopeye@users.noreply.github.com>
2023-03-23 11:53:30 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,14 @@ import store from '@/app/store/store';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import TagManager from 'react-gtm-module'
 
 function MyApp({ Component, pageProps }: any) {
   firebaseInitialization();
+TagManager.initialize({
+  gtmId: 'G-WV41H07YJR'
+})
+
 
   return (
     <Provider store={store}>

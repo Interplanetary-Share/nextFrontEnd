bfa04fb93cbdc80fb9e4e37f5ff3f35245be9f4a
kenneth suarez <33601506+polopopeye@users.noreply.github.com>
2023-05-20 14:48:46 +0200

    restore to prevoiour

--- src/app/store/slices/ipfs/local/ipfs.action.ts
+++ src/app/store/slices/ipfs/local/ipfs.action.ts
@@ -30,8 +30,8 @@ export const addFileToIPFS = createAsyncThunk(
 
     const infoFile = await ipfs
       .add(blob, {
-        // chunker: 'size-6000000', // Best performance for large files
-        chunker: 'size-3000000', // HAD SOME PROBLEMS WITH PHONES
+        chunker: 'size-6000000', // Best performance for large files
+        // chunker: 'size-3000000', // HAD SOME PROBLEMS WITH PHONES
         // chunker: 'size-1000',
         onlyHash: false,
         pin: true,

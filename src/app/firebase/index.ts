0461135bdc464fa82b79cf9b849360e20ee29626
kenneth3292922 <kenneth@unionavatars.com>
2023-09-17 23:41:44 +0200

    Update index.ts

--- src/app/firebase/index.ts
+++ src/app/firebase/index.ts
@@ -1,4 +1,5 @@
-import { initializeApp } from 'firebase/app';
+/* eslint-disable */
+import { initializeApp } from 'firebase/app'
 const firebaseInitialization = () => {
   initializeApp({
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -8,7 +9,7 @@ const firebaseInitialization = () => {
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-  });
-};
+  })
+}
 
-export default firebaseInitialization;
+export default firebaseInitialization

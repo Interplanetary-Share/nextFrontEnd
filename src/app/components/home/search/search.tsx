cd06f440c008b226410c97bfd0c029c177cc8eea
kenneth3292922 <kenneth@unionavatars.com>
2023-09-17 23:36:38 +0200

    Update search.tsx

--- src/app/components/home/search/search.tsx
+++ src/app/components/home/search/search.tsx
@@ -1,8 +1,6 @@
 import { setSearchString } from '@/app/store/slices/files/query.slice'
 import { useDispatch } from 'react-redux'
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
-
 const Search = () => {
   const dispatch = useDispatch()
   const handleSearch = (e: any) => {
@@ -20,8 +18,21 @@ const Search = () => {
             handleSearch(e)
           }}
         />
-        <button className="btn btn-square rounded-r-lg mr-5 p-2">
-          <MagnifyingGlassIcon />
+        <button className="btn btn-square">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
         </button>
       </div>
     </div>

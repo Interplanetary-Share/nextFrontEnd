ac00a939c71a5531f56c6f6efdbdc385cf56591d
kenneth3292922 <kenneth@unionavatars.com>
2023-09-17 22:35:22 +0200

    update

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const nextConfig = {
   future: { webpack5: true },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
-    return config;
+    config.resolve.alias.canvas = false
+    config.resolve.alias.encoding = false
+    return config
   },
   experimental: {
     appDir: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig

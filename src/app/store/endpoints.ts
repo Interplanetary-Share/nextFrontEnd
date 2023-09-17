5cff5a06e58400b2d26dcd68289382ce56c25c28
kenneth3292922 <kenneth@unionavatars.com>
2023-09-17 22:17:21 +0200

    :coffin: delete unused code

--- src/app/store/endpoints.ts
+++
@@ -1,4 +0,0 @@
-// TODO: add report suport
-// const reportController = '/report'
-// export const apiReportCreate = apiHostname + reportController + '/create/'
-// export const apiReportDelete = apiHostname + reportController + '/delete/'
